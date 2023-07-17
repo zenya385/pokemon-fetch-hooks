f7f1fe63f1c8987086f19bd7f6a373bad4006309
Zeno Roman <zeno385@gmail.com>
2023-07-17 19:02:39 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,18 @@
-import { Component } from 'react';
 import PokemonForm from 'PokemonForm';
 import PokemonInfo from 'PokemonInfo';
-// import { ToastContainer } from 'react-toastify';
 
-export default class App extends Component {
-  state = {
-    pokemonName: '',
+import { useState } from 'react';
+
+export default function App() {
+  const [pokemonName, setPokemonName] = useState('');
+  const handleFormSubmit = pokemonName => {
+    setPokemonName(pokemonName);
   };
-
-  handleFormSubmit = pokemonName => {
-    this.setState({ pokemonName });
-  };
-
-  render() {
-    return (
-      <>
-        <h1>Pokemonu</h1>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
-        {/* <ToastContainer position="top-right" autoClose={5000} /> */}
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Pokemonu</h1>
+      <PokemonForm onSubmit={handleFormSubmit} />
+      <PokemonInfo pokemonName={pokemonName} />
+    </>
+  );
 }
